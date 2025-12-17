2be50d825e425f933c08902816c05abc7e23e4c5
DeNt <mrfraos@gmail.com>
2025-12-17 15:41:17 +0200

    testing

---
+++ bazingaBE1/src/main/java/com/example/bazingabe1/BazingaBe1Application.java
@@ -0,0 +1,13 @@
+package com.example.bazingabe1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazingaBe1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazingaBe1Application.class, args);
+    }
+
+}
---
+++ bazingaBE1/src/test/java/com/example/bazingabe1/BazingaBe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bazingabe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazingaBe1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
